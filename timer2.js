b780c512a3ee9eb695cd9d2064db00d97b1f15c7
Joseph Williams <josephwilliamsjs@gmail.com>
2020-12-02 01:12:45 +0000

    added more interactive features to timer

---
+++ timer2.js
@@ -0,0 +1,34 @@
+const { clear } = require('console');
+const process = require('process');
+const readline = require('readline')
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+process.stdin.setEncoding('utf8');
+
+rl.on('line', (data) => {
+    const counter = data * 1000;
+    process.stdout.write(`setting timer for ${data} seconds`);
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, counter)
+  },
+);
+
+rl.on('SIGINT', () => {
+  process.stdout.write('Thanks for using me, ciao!\n');
+  rl.close();
+})
+
+process.stdin.on('keypress', (c) => {
+ if (c == 'b') {
+  process.stdout.write('\x07');
+}
+})
+
+
+
